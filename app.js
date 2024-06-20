e76560f7261498b744f884ab0efabed551b159e0
SamPrestoo <160303378+SamPrestoo@users.noreply.github.com>
2024-06-20 17:50:00 -0500

    Update app.js

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ tl.to('.slider', {y: '-100%', duration: 1.5, delay: 0.5 });
 tl.to('.intro', {y: '-100%', duration: 1 }, '-=1');
 tl.fromTo('nav', {opacity: 0}, {opacity: 1, duration: 1 });
 tl.fromTo('h1', {opacity: 0}, {opacity: 1, duration: 1 }, '-=1');
-tl.fromTo('.quickLinks', {opacity: 0}, {opacity: 1, duration: 1 }, '-=1');
+tl.fromTo('.get-quote', {opacity: 0}, {opacity: 1, duration: 1 }, '-=1');
 
 
 const toTop = () => window.scrollTo({top: 0, behavior: 'smooth'});
