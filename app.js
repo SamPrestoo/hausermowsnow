53c7b43abe41dc8f94562055618ed35bd4205a52
SamPrestoo <160303378+SamPrestoo@users.noreply.github.com>
2024-08-02 12:17:36 -0500

    Update app.js

--- app.js
+++ app.js
@@ -10,33 +10,33 @@ hamburgerDesign.addEventListener('click', () => {
 
 
 
-window.addEventListener('scroll', function() {
-  const fadeIns = document.querySelectorAll('#fade-in');
-  for (let i = 0; i < fadeIns.length; i++) {
-    const fadeIn = fadeIns[i];
-    const fadeInPosition = fadeIn.getBoundingClientRect().top + fadeIn.offsetHeight / 2;
-    const screenPosition = window.innerHeight;
-    if (fadeInPosition < screenPosition) {
-      fadeIn.style.opacity = '1';
-    }
-  }
-});
+// window.addEventListener('scroll', function() {
+//   const fadeIns = document.querySelectorAll('#fade-in');
+//   for (let i = 0; i < fadeIns.length; i++) {
+//     const fadeIn = fadeIns[i];
+//     const fadeInPosition = fadeIn.getBoundingClientRect().top + fadeIn.offsetHeight / 2;
+//     const screenPosition = window.innerHeight;
+//     if (fadeInPosition < screenPosition) {
+//       fadeIn.style.opacity = '1';
+//     }
+//   }
+// });
 
 
 
 
 
-const tl = gsap.timeline({defaults: { ease: 'power1.out' } });
+// const tl = gsap.timeline({defaults: { ease: 'power1.out' } });
 
-tl.to('.text', {y:'0%', duration: 1, stagger: 0.25 });
-tl.to('.slider', {y: '-100%', duration: 1.5, delay: 0.5 });
-tl.to('.intro', {y: '-100%', duration: 1 }, '-=1');
-tl.fromTo('nav', {opacity: 0}, {opacity: 1, duration: 1 });
-tl.fromTo('h1', {opacity: 0}, {opacity: 1, duration: 1 }, '-=1');
-tl.fromTo('.get-quote', {opacity: 0}, {opacity: 1, duration: 1 }, '-=1');
+// tl.to('.text', {y:'0%', duration: 1, stagger: 0.25 });
+// tl.to('.slider', {y: '-100%', duration: 1.5, delay: 0.5 });
+// tl.to('.intro', {y: '-100%', duration: 1 }, '-=1');
+// tl.fromTo('nav', {opacity: 0}, {opacity: 1, duration: 1 });
+// tl.fromTo('h1', {opacity: 0}, {opacity: 1, duration: 1 }, '-=1');
+// tl.fromTo('.get-quote', {opacity: 0}, {opacity: 1, duration: 1 }, '-=1');
 
 
-const toTop = () => window.scrollTo({top: 0, behavior: 'smooth'});
+// const toTop = () => window.scrollTo({top: 0, behavior: 'smooth'});
 
 
 
